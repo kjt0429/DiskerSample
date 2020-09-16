fac3f29d70f3993cc03f823b0d01205f141928c2
disker <disker@gamevilcom2us.com>
2020-09-16 17:25:54 +0900

    [Tablayout] 내용 추가

--- TabLayoutSample/app/src/main/java/com/disker/tablayoutsample/MainActivity.java
+++ TabLayoutSample/app/src/main/java/com/disker/tablayoutsample/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+
+		// 코드를 통한 추가 방식
+		TabLayout tabLayout = findViewById(R.id.tabLayout1);
+
+		TabLayout.Tab tab1 = tabLayout.newTab();
+		tab1.setText("tab1");
+
+		TabLayout.Tab tab2 = tabLayout.newTab();
+		tab2.setText("tab1");
+
+		tabLayout.addTab(tab1);
+		tabLayout.addTab(tab2);
 	}
 }
